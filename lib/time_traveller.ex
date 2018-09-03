e75720f8d96dc50e8ce85dba1db533eb3006c9da
Lance Petersen <lance@goskip.com>
2018-09-03 17:23:55 -0600

    Making interface cleaner

--- lib/time_traveller.ex
+++ lib/time_traveller.ex
@@ -6,8 +6,9 @@ defmodule TimeTraveller do
 
   @doc """
   Returns the UTC representation of the beginning of the day for the given date
-  and timezone. The date is a string in the format "YYYY-MM-DD". The date string
-  is parsed as being in the timezone specified and the UTC datetime is returned.
+  and timezone. The date can be a string in the format "YYYY-MM-DD" or a Date, DateTime,
+  or NaiveDateTime struct. The date is interpreted as being in the timezone specified
+  and the UTC datetime is returned.
 
   ## Examples
 
@@ -15,30 +16,36 @@ defmodule TimeTraveller do
       #DateTime<2018-09-01 06:00:00Z>
 
   """
-  @spec beginning_of_day_utc(String.t(), String.t()) :: DateTime.t()
-  def beginning_of_day_utc(date, timezone) when is_binary(date) do
+  @spec beginning_of_day_utc(String.t() | Date.t() | NaiveDateTime.t() | DateTime.t(), String.t()) ::
+          DateTime.t()
+  def beginning_of_day_utc(date, timezone) do
+    _beginning_of_day_utc(date, timezone)
+  end
+
+  @spec _beginning_of_day_utc(String.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_day_utc(date, timezone) when is_binary(date) do
     date
     |> Timex.parse!("{YYYY}-{0M}-{0D}")
     |> naive_utc(timezone)
   end
 
-  @spec beginning_of_day_utc(Date.t(), String.t()) :: DateTime.t()
-  def beginning_of_day_utc(%Date{} = date, timezone) when is_binary(timezone) do
+  @spec _beginning_of_day_utc(Date.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_day_utc(%Date{} = date, timezone) when is_binary(timezone) do
     date
     |> Timex.to_datetime(timezone)
     |> Timex.to_datetime("UTC")
   end
 
-  @spec beginning_of_day_utc(NaiveDateTime.t(), String.t()) :: DateTime.t()
-  def beginning_of_day_utc(%NaiveDateTime{} = datetime, timezone) do
+  @spec _beginning_of_day_utc(NaiveDateTime.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_day_utc(%NaiveDateTime{} = datetime, timezone) do
     datetime
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_day()
     |> Timex.to_datetime("UTC")
   end
 
-  @spec beginning_of_day_utc(DateTime.t(), String.t()) :: DateTime.t()
-  def beginning_of_day_utc(%DateTime{} = datetime, timezone) do
+  @spec _beginning_of_day_utc(DateTime.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_day_utc(%DateTime{} = datetime, timezone) do
     datetime
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_day()
@@ -46,7 +53,10 @@ defmodule TimeTraveller do
   end
 
   @doc """
-  beginning_of_week_utc
+  Returns the UTC representation of the beginning of the week for the given date
+  and timezone. The date can be a string in the format "YYYY-MM-DD" or a Date, DateTime,
+  or NaiveDateTime struct. The date is interpreted as being in the timezone specified
+  and the UTC datetime is returned.
 
   ## Examples
 
@@ -54,34 +64,50 @@ defmodule TimeTraveller do
       #DateTime<2018-08-26 06:00:00Z>
 
   """
-  def beginning_of_week_utc(date, timezone, week_start \\ :sun)
+  @spec beginning_of_week_utc(
+          String.t() | Date.t() | NaiveDateTime.t() | DateTime.t(),
+          String.t()
+        ) :: DateTime.t()
+  def beginning_of_week_utc(date, timezone) do
+    _beginning_of_week_utc(date, timezone, :sun)
+  end
 
-  @spec beginning_of_week_utc(String.t(), String.t(), atom()) :: DateTime.t()
-  def beginning_of_week_utc(date, timezone, week_start) when is_binary(date) do
+  @spec beginning_of_week_utc(
+          String.t() | Date.t() | NaiveDateTime.t() | DateTime.t(),
+          String.t(),
+          atom()
+        ) :: DateTime.t()
+  def beginning_of_week_utc(date, timezone, week_start) do
+    _beginning_of_week_utc(date, timezone, week_start)
+  end
+
+  @spec _beginning_of_week_utc(String.t(), String.t(), atom()) :: DateTime.t()
+  defp _beginning_of_week_utc(date, timezone, week_start) when is_binary(date) do
     date
     |> Timex.parse!("{YYYY}-{0M}-{0D}")
     |> Timex.beginning_of_week(week_start)
     |> naive_utc(timezone)
   end
 
-  @spec beginning_of_week_utc(Date.t(), String.t(), atom()) :: DateTime.t()
-  def beginning_of_week_utc(%Date{} = date, timezone, week_start)
-      when is_binary(timezone) and is_atom(week_start) do
+  @spec _beginning_of_week_utc(Date.t(), String.t(), atom()) :: DateTime.t()
+  defp _beginning_of_week_utc(%Date{} = date, timezone, week_start)
+       when is_binary(timezone) and is_atom(week_start) do
     date
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_week(week_start)
     |> Timex.to_datetime("UTC")
   end
 
-  @spec beginning_of_week_utc(NaiveDateTime.t(), String.t(), atom()) :: DateTime.t()
-  def beginning_of_week_utc(%NaiveDateTime{} = datetime, timezone, week_start) do
+  @spec _beginning_of_week_utc(NaiveDateTime.t(), String.t(), atom()) :: DateTime.t()
+  defp _beginning_of_week_utc(%NaiveDateTime{} = datetime, timezone, week_start) do
     datetime
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_week(week_start)
     |> Timex.to_datetime("UTC")
   end
 
-  def beginning_of_week_utc(%DateTime{} = datetime, timezone, week_start) do
+  @spec _beginning_of_week_utc(DateTime.t(), String.t(), atom()) :: DateTime.t()
+  defp _beginning_of_week_utc(%DateTime{} = datetime, timezone, week_start) do
     datetime
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_week(week_start)
@@ -89,7 +115,10 @@ defmodule TimeTraveller do
   end
 
   @doc """
-  beginning_of_month_utc
+  Returns the UTC representation of the beginning of the month for the given date
+  and timezone. The date can be a string in the format "YYYY-MM-DD" or a Date, DateTime,
+  or NaiveDateTime struct. The date is interpreted as being in the timezone specified
+  and the UTC datetime is returned.
 
   ## Examples
 
@@ -97,39 +126,51 @@ defmodule TimeTraveller do
       #DateTime<2018-09-01 06:00:00Z>
 
   """
-  @spec beginning_of_month_utc(String.t(), String.t()) :: DateTime.t()
-  def beginning_of_month_utc(date, timezone) when is_binary(date) do
+  @spec beginning_of_month_utc(
+          String.t() | Date.t() | NaiveDateTime.t() | DateTime.t(),
+          String.t()
+        ) :: DateTime.t()
+  def beginning_of_month_utc(date, timezone) do
+    _beginning_of_month_utc(date, timezone)
+  end
+
+  @spec _beginning_of_month_utc(String.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_month_utc(date, timezone) when is_binary(date) do
     date
     |> Timex.parse!("{YYYY}-{0M}-{0D}")
     |> Timex.beginning_of_month()
     |> naive_utc(timezone)
   end
 
-  @spec beginning_of_month_utc(Date.t(), String.t()) :: DateTime.t()
-  def beginning_of_month_utc(%Date{} = date, timezone) when is_binary(timezone) do
+  @spec _beginning_of_month_utc(Date.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_month_utc(%Date{} = date, timezone) when is_binary(timezone) do
     date
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_month()
     |> Timex.to_datetime("UTC")
   end
 
-  @spec beginning_of_month_utc(NaiveDateTime.t(), String.t()) :: DateTime.t()
-  def beginning_of_month_utc(%NaiveDateTime{} = datetime, timezone) do
+  @spec _beginning_of_month_utc(NaiveDateTime.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_month_utc(%NaiveDateTime{} = datetime, timezone) do
     datetime
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_month()
     |> Timex.to_datetime("UTC")
   end
 
-  @spec beginning_of_month_utc(DateTime.t(), String.t()) :: DateTime.t()
-  def beginning_of_month_utc(%DateTime{} = datetime) do
+  @spec _beginning_of_month_utc(DateTime.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_month_utc(%DateTime{} = datetime, timezone) do
     datetime
+    |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_month()
     |> Timex.to_datetime("UTC")
   end
 
   @doc """
-  beginning_of_year_utc
+  Returns the UTC representation of the beginning of the year for the given date
+  and timezone. The date can be a string in the format "YYYY-MM-DD" or a Date, DateTime,
+  or NaiveDateTime struct. The date is interpreted as being in the timezone specified
+  and the UTC datetime is returned.
 
   ## Examples
 
@@ -137,37 +178,57 @@ defmodule TimeTraveller do
       #DateTime<2018-01-01 07:00:00Z>
 
   """
-  @spec beginning_of_year_utc(String.t(), String.t()) :: DateTime.t()
-  def beginning_of_year_utc(date, timezone) when is_binary(date) do
+  @spec beginning_of_year_utc(
+          String.t() | Date.t() | NaiveDateTime.t() | DateTime.t(),
+          String.t()
+        ) :: DateTime.t()
+  def beginning_of_year_utc(date, timezone) do
+    _beginning_of_year_utc(date, timezone)
+  end
+
+  @spec _beginning_of_year_utc(String.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_year_utc(date, timezone) when is_binary(date) do
     date
     |> Timex.parse!("{YYYY}-{0M}-{0D}")
     |> Timex.beginning_of_year()
     |> naive_utc(timezone)
   end
 
-  @spec beginning_of_year_utc(Date.t(), String.t()) :: DateTime.t()
-  def beginning_of_year_utc(%Date{} = date, timezone) when is_binary(timezone) do
+  @spec _beginning_of_year_utc(Date.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_year_utc(%Date{} = date, timezone) when is_binary(timezone) do
     date
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_year()
     |> Timex.to_datetime("UTC")
   end
 
-  @spec beginning_of_year_utc(NaiveDateTime.t(), String.t()) :: DateTime.t()
-  def beginning_of_year_utc(%NaiveDateTime{} = datetime, timezone) do
+  @spec _beginning_of_year_utc(NaiveDateTime.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_year_utc(%NaiveDateTime{} = datetime, timezone) do
     datetime
     |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_year()
     |> Timex.to_datetime("UTC")
   end
 
-  @spec beginning_of_year_utc(DateTime.t(), String.t()) :: DateTime.t()
-  def beginning_of_year_utc(%DateTime{} = datetime) do
+  @spec _beginning_of_year_utc(DateTime.t(), String.t()) :: DateTime.t()
+  defp _beginning_of_year_utc(%DateTime{} = datetime, timezone) do
     datetime
+    |> Timex.to_datetime(timezone)
     |> Timex.beginning_of_year()
     |> Timex.to_datetime("UTC")
   end
 
+  @doc """
+  Converts a NaiveDateTime to the specified timezone and then
+  converts that to UTC.
+
+  ## Examples
+
+      iex> ndt = "2018-09-03" |> Timex.parse!("{YYYY}-{0M}-{0D}")
+      iex> TimeTraveller.naive_utc(ndt, "America/Denver")
+      #DateTime<2018-09-03 06:00:00Z>
+
+  """
   @spec naive_utc(NaiveDateTime.t(), String.t()) :: DateTime.t()
   def naive_utc(%NaiveDateTime{} = naive_datetime, timezone) do
     naive_datetime
